7349565b255a273daa3f67aa23d5296ceacceced
Michael Yu <1619025+acrylix@users.noreply.github.com>
2020-11-20 12:05:08 -0500

    upgrade to hardhat

---
+++ test/utils/index.ts
@@ -0,0 +1,17 @@
+import * as chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+const ChaiBigNumber = require('chai-bignumber')
+import chaiString from 'chai-string'
+import * as ethers from 'ethers'
+
+export * from './contract'
+export * from './helpers'
+
+const BigNumber = ethers.BigNumber
+export { BigNumber }
+
+export const { assert, expect } = chai
+  .use(chaiString)
+  .use(chaiAsPromised)
+  .use(ChaiBigNumber())
+  .use(require('chai-bignumber')(BigNumber));
